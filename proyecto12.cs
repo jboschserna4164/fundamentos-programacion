7f69411b8c3683c44beed262fe62e65b5f78b57c
jboschserna4164 <62222010+jboschserna4164@users.noreply.github.com>
2020-07-16 18:00:47 -0500

    Add files via upload

---
+++ proyecto12.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace proyecto12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pedimos el peso y la altura al usuario
+            Console.WriteLine("Ingrese su peso en kg");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su altura en m");
+            double altura = double.Parse(Console.ReadLine());
+
+            //convertimos la altura en m cuadrados
+            double altura1 = altura * altura;
+
+            //hallamos el IMC
+            double imc = peso / altura1;
+            Console.WriteLine("Su indice de masa corporal es " + imc);
+
+            if(imc < 18.5)
+            {
+                Console.WriteLine("Esta bajo de peso");
+            }
+
+            if(imc >= 18.5 && imc < 25)
+            {
+                Console.WriteLine("Esta normal de peso");
+            }
+
+            if (imc >= 25 && imc < 30)
+            {
+                Console.WriteLine("Esta en sobrepeso");
+            }
+
+            if(imc >= 30)
+            {
+                Console.WriteLine("Esta en obesidad");
+            }
+
+        }
+    }
+}
---
+++ proyecto13.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace proyecto13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pedimos al usuario a,b y c
+            Console.WriteLine("Ingrese a");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese b");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese c");
+            double c = double.Parse(Console.ReadLine());
+
+            double discriminante = Math.Sqrt((b * b) - (4 * a * c));
+
+            double solucion = (-b + discriminante) / (2*a);
+            double solucion1 = (-b - discriminante) / (2 * a);
+
+            if(discriminante == 0)
+            {
+                Console.WriteLine(" X = " + solucion1);
+            }
+
+            if(discriminante > 0)
+            {
+                Console.WriteLine("Hay 2 tipos de solucion = " + "X1=" + solucion + " X2=" + solucion1);
+            }
+
+            if(discriminante < 0)
+            {
+                Console.WriteLine("No es posible calcular la solucion");
+            }
+
+        }
+    }
+}
