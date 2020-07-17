dc23aecd356e6541a09354f3841775319f0ac3a0
jboschserna4164 <62222010+jboschserna4164@users.noreply.github.com>
2020-07-17 16:09:44 -0500

    Add files via upload

--- proyecto10.cs
+++ proyecto10.cs
@@ -11,28 +11,42 @@ namespace proyecto10
             Console.WriteLine("Ingrese su salario");
             double salario = int.Parse(Console.ReadLine());
 
-            double salariomin = salario / 877803;
-            Console.WriteLine(salariomin + " SMMLV");
+            //calcular el salario minimo mensual
+            double salariominm = salario / 877803;
+            Console.WriteLine(salariominm + " SMMLV");
 
-            if(salariomin >= 0 && salariomin < 2)
+            //calcular el salario minimo diario
+            double salariomind = 877803 / 30;
+            Console.WriteLine("El salario minimo diario es " + salariomind);
+
+            
+
+            if(salariominm >= 0 && salariominm < 2)
             {
                 Console.WriteLine("Es tarifa A");
                 Console.WriteLine("Cuota moderadora = el 11.7% de un salario mínimo diario legal vigente.");
+                double cuota = salariomind * 0.117;
+                Console.WriteLine("Cuota moderadora = " + cuota);
+
             }
 
-            if (salariomin >= 2 && salariomin < 5)
+            if (salariominm >= 2 && salariominm < 5)
             {
                 Console.WriteLine("Es tarifa B");
                 Console.WriteLine("Cuota moderadora = el 46.1% de un salario mínimo diario legal vigente.");
+                double cuota = salariomind * 0.461;
+                Console.WriteLine("Cuota moderadora = " + cuota);
             }
 
-            if (salariomin >= 5)
+            if (salariominm >= 5)
             {
                 Console.WriteLine("Es tarifa C");
                 Console.WriteLine("Cuota moderadora = el 121.5% de un salario mínimo diario legal vigente.");
+                double cuota = salariomind * 0.1215;
+                Console.WriteLine("Cuota moderadora = " + cuota);
             }
 
-            else if(salariomin < 0)
+            else if(salariominm < 0)
             {
                 Console.WriteLine("Salario no no permitido");
             }
